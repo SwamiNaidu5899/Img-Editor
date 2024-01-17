a2a3777993392859ae6fba66d29e42c20738b488
Swami Naidu Datti <152952658+SwamiNaidu5899@users.noreply.github.com>
2024-01-17 22:47:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+function btn1()
+{
+    // document.getElementById("img-new").style.display= "none";
+    document.getElementById("img-new").style.filter= "brightness(50%)";
+    document.getElementById("btn-1").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function btn2()
+{
+    document.getElementById("img-new").style.filter= "blur(5px)";
+    document.getElementById("btn-2").style.background= "#3a6fda";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function btn3()
+{
+    document.getElementById("img-new").style.filter= "contrast(80%)";
+    document.getElementById("btn-3").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function btn4()
+{
+    document.getElementById("img-new").style.filter= "grayscale(50%)";
+    document.getElementById("btn-4").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function btn5()
+{
+    document.getElementById("img-new").style.rotate= "90deg";
+    document.getElementById("btn-5").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function btn6()
+{
+    document.getElementById("img-new").style.rotate= "-90deg";
+    document.getElementById("btn-6").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function btn7()
+{
+    document.getElementById("img-new").style.rotate= "180deg";
+    document.getElementById("btn-7").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function btn8()
+{
+    document.getElementById("img-new").style.rotate= "0deg";
+    document.getElementById("btn-8").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("btn-9").style.background= "white";
+}
+function reset()
+{
+    document.getElementById("btn-9").style.background= "#3a6fda";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+    document.getElementById("img-new").style.filter= "grayscale(100%)";
+    document.getElementById("img-new").style.rotate= "0deg";
+    document.getElementById("img-new").style.rotate= "0deg";
+    document.getElementById("img-new").style.rotate= "0deg";
+    document.getElementById("img-new").style.rotate= "0deg";
+    document.getElementById("img-new").style.filter= "brightness(100%)";
+    document.getElementById("img-new").style.filter= "blur(0)";
+    document.getElementById("img-new").style.filter= "contrast(100%)";
+    document.getElementById("btn-8").style.background= "white";
+    document.getElementById("btn-2").style.background= "white";
+    document.getElementById("btn-3").style.background= "white";
+    document.getElementById("btn-4").style.background= "white";
+    document.getElementById("btn-5").style.background= "white";
+    document.getElementById("btn-6").style.background= "white";
+    document.getElementById("btn-7").style.background= "white";
+    document.getElementById("btn-1").style.background= "white";
+}
+
+function cImg()
+{
+    alert("idhi work avvadhu mowaa!!!!")
+}
+
+function sImg()
+{
+    alert("idhi work avvadhu mowaa!!!!!!!!1")
+}
